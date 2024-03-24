70a2bbb9ce6df3437912e70e8a3a979289de7f51
dhruval <dhruvalcd@gmail.com>
2024-03-24 18:21:24 -0400

    first commit

--- App.js
+++ App.js
@@ -1,18 +1,61 @@
-import { StyleSheet, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Platform, ScrollView} from 'react-native';
+import PokemonCard from "./components/PokemonCard";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
 
-    </View>
-  );
+    const charmanderData = {
+        name: "Charmander",
+        image: require("./assets/charmander.png"),
+        type: "Fire",
+        hp: 39,
+        moves: ["Scratch", "Ember", "Growl", "Leer"],
+        weaknesses: ["Water", "Rock"],
+    };
+
+    const bulbasaurData = {
+        name: "Bulbasaur",
+        image: require("./assets/bulbasaur.png"),
+        type: "Grass",
+        hp: 45,
+        moves: ["Tackle", "Growl", "Leech Seed", "Vine Whip"],
+        weaknesses: ["Fire", "Flying", "Ice", "Psychic"],
+    };
+
+    const squirtleData = {
+        name: "Squirtle",
+        image: require("./assets/squirtle.png"),
+        type: "Water",
+        hp: 44,
+        moves: ["Tackle", "Tail Whip", "Bubble", "Water Gun"],
+        weaknesses: ["Electric", "Grass"],
+    }
+    const pikachuData = {
+        name: "Pikachu",
+        image: require("./assets/pikachu.png"),
+        type: "Electric",
+        hp: 35,
+        moves: ["Thundershock", "Growl", "Tail Whip", "Quick Attack"],
+        weaknesses: ["Ground"],
+    }
+
+
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <ScrollView>
+                <PokemonCard {...charmanderData}/>
+                <PokemonCard {...bulbasaurData}/>
+                <PokemonCard {...squirtleData}/>
+                <PokemonCard {...pikachuData}/>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        paddingTop: Platform.OS === 'android' ? 30 : 0,
+    },
 });
---
+++ components/PokemonCard.js
@@ -0,0 +1,130 @@
+import {View, Text, StyleSheet, Platform, Image} from "react-native";
+
+
+const getTypeDetails = (type) => {
+    switch (type.toLowerCase()) {
+        case "fire":
+            return {borderColor: "red", emoji: "🔥"};
+        case "water":
+            return {borderColor: "blue", emoji: "💧"};
+        case "grass":
+            return {borderColor: "green", emoji: "🌿"};
+        case "electric":
+            return {borderColor: "yellow", emoji: "⚡"};
+        default:
+            return {borderColor: "gray", emoji: "❓"};
+    }
+}
+
+
+export default function PokemonCard(
+    {
+        name, image, type, hp, moves, weaknesses
+    }
+) {
+
+    const {borderColor, emoji} = getTypeDetails(type);
+
+    return (
+        <View style={styles.card}>
+            <View style={styles.nameContainer}>
+                <Text style={styles.name}>{name}</Text>
+                <Text style={styles.hp}>❤{hp}</Text>
+            </View>
+
+            <Image source={image} style={styles.image} accessibilityLabel={`${name} pokemon`} resizeMode="contain"/>
+
+            <View style={styles.typeContainer}>
+                <View style={[styles.badge, {borderColor: borderColor}]}>
+                    <Text style={styles.typeEmoji}>{emoji}</Text>
+                    <Text style={styles.typeText}>{type}</Text>
+                </View>
+            </View>
+
+            <View style={styles.movesContainer}>
+                <Text style={styles.movesText}>Moves: {moves.join(", ")}</Text>
+            </View>
+
+            <View style={styles.weaknessContainer}>
+                <Text style={styles.weaknessText}>Weaknesses: {weaknesses.join(", ")}</Text>
+            </View>
+
+
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: 'white', borderRadius: 16, borderWidth: 2, padding: 16, margin: 16, ...Platform.select({
+            ios: {
+                shadowColor: '#333', shadowOffset: {width: 2, height: 2}, shadowOpacity: 0.3, shadowRadius: 4,
+            }, android: {
+                elevation: 5,
+            },
+        }),
+    },
+
+    nameContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 32,
+    },
+    name: {
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    hp: {
+        fontSize: 22,
+    },
+    image: {
+        width: "100%",
+        height: 200,
+        marginBottom: 16,
+    },
+
+    typeContainer: {
+        alignItems: 'center',
+        marginBottom: 40,
+    },
+    badge: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingVertical: 6,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+        borderWidth: 4,
+    },
+    typeEmoji: {
+        fontSize: 30,
+        marginRight: 12,
+    },
+    typeText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+    movesContainer: {
+        marginBottom: 16,
+    },
+    movesText: {
+        fontSize: 22,
+        fontWeight: 'bold'
+    },
+    weaknessContainer: {
+        marginBottom: 8,
+    },
+    weaknessText: {
+        fontSize: 22,
+        fontWeight: 'bold'
+    },
+})
+
+
+// const PokemonCard = ({data}) => (
+//     <View style={styles.card}>
+//         <Text>
+//             {data.name}
+//         </Text>
+//     </View>
+// )
