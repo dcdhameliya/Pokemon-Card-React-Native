1002347260ba02d30860898dd1341f5bc053a4dc
dhruval <dhruvalcd@gmail.com>
2024-03-24 17:40:32 -0400

    Initial commit
    Generated by create-expo-app 2.1.1.

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
